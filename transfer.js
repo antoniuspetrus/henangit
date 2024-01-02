568c0e398f26575b11300f6e581889b6ced450cc
antoniuspetrus <antoniuspetruss@yahoo.com>
2024-01-02 20:30:50 +0700

    f_trf_different_bank

--- transfer.js
+++ transfer.js
@@ -5,7 +5,12 @@ function Function123() {
     console.log("123")
 }
 
-
+function transferDifferentBank () {
+    console.log("696")
+    console.log("696")
+    console.log("696")
+    console.log("696")
+}
 
 function Function999() {
     console.log("zxc")

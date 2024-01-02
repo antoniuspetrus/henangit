f35ccdfe1c49f5c7fa3f859161e9b864bc93a939
antoniuspetrus <antoniuspetruss@yahoo.com>
2024-01-02 20:08:33 +0700

    f_check_balance

---
+++ transfer.js
@@ -0,0 +1,17 @@
+function Function123() {
+    console.log("123")
+    console.log("123")
+    console.log("123")
+    console.log("123")
+}
+
+
+
+function Function999() {
+    console.log("zxc")
+
+    console.log("zxc")
+    console.log("zxc")
+    console.log("zxc")
+    console.log("zxc")
+}
